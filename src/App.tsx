9547b3b1f80c8e790cf5addabfc36a83e3dbaa65
kynaios <blaise.witczak@gmail.com>
2023-10-11 12:44:52 +0200

    Prettier - code format

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
-import { useState } from 'react'
-import './styles/main.scss'
+import { useState } from 'react';
+import './styles/main.scss';
 
 function App() {
-
   function toggleTheme() {
-    document.body.classList.toggle('dark')
+    document.body.classList.toggle('dark');
   }
 
-  return <button onClick={toggleTheme}>Theme Toggle</button>
+  return <button onClick={toggleTheme}>Theme Toggle</button>;
 }
 
-export default App
+export default App;
