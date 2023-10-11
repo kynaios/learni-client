c5168be2ba0dd3ffd6657cfb1b3dffdb5a8ce236
kynaios <blaise.witczak@gmail.com>
2023-10-11 14:09:28 +0200

    Create Card layout

---
+++ src/components/Card.tsx
@@ -0,0 +1,12 @@
+import '../styles/card.scss';
+
+function Card() {
+  return (
+    <div className='flash-card'>
+      <input className='word-input l' />
+      <span className='translation l'>Lorem ipsum</span>
+    </div>
+  );
+}
+
+export default Card;
