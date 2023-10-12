944562877ae8c2b6eef266ebb749383c756e2784
kynaios <blaise.witczak@gmail.com>
2023-10-12 21:15:16 +0200

    Add dropdown to card

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,4 +1,5 @@
 import { ChangeEvent, useState } from 'react';
+import Dropdown from './Dropdown';
 import axios from 'axios';
 import '../styles/card.scss';
 
@@ -28,13 +29,14 @@ async function translateTextWithDeepL(word: string, targetLang: string): Promise
 function Card({ addFlashcard }) {
   const [word, setWord] = useState<string>('');
   const [translation, setTranslation] = useState<string>('');
+  const [language, setLanguage] = useState<string>('');
 
   const handleWorld = async (event: ChangeEvent<HTMLInputElement>) => {
     const { value } = event.target;
     setWord(value);
 
     if (value) {
-      const res = await translateTextWithDeepL(value, 'en-US');
+      const res = await translateTextWithDeepL(value, language);
 
       if (res) {
         setTranslation(res);
@@ -47,6 +49,7 @@ function Card({ addFlashcard }) {
 
   return (
     <div className='flash-card'>
+      <Dropdown setLanguage={setLanguage} />
       <input onChange={handleWorld} value={word} className='word-input l' />
       <span className='translation l'>{translation}</span>
       <button onClick={() => addFlashcard({ word, translation })}>Add</button>
